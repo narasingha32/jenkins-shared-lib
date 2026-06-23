6e691fb6129e76d66cb1be30f5992145ec4268a4
Narasingha <122041081+narasingha32@users.noreply.github.com>
2026-06-23 19:06:27 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,1 +1,8 @@
-
+def call(String project, String tag){
+withCredentials([usernamePassword('credentialsId':"dockerhubcred",passwordVariable:"dockerhubpass",usernameVariable:"dockerhubuser")])
+  {
+    sh "docker login -u ${env.dockerhubuser} -p ${env.dockerhubpass}"
+    sh "docker tag ${project}-${tag}  ${env.dockerhubuser}/wonderlust-${tag}"
+    sh "docker push ${env.dockerhubuser}/wonderlust-${tag}"
+  }
+}
